0f4acc88643b5fff4635df37c263252d8db1ac57
mikebratt <mike.bratt@gmail.com>
2023-02-02 21:00:29 +0000

    added lnav and pyenv prereqs

--- setup.sh
+++ setup.sh
@@ -2,3 +2,8 @@
 
 sudo apt update
 sudo apt install thunar-archive-plugin 
+sudo apt install lnav
+# prerequisites for pyenv # curl https://pyenv.run | bash
+sudo apt update; sudo apt install build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
